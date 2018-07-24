e13695c8c480989b93393e18f38c55ae5de3aac4
Chris0King <790132154@qq.com>
2018-07-24 12:40:05 +0800

    Update RouteConfig.cs
    新加两个参数的url规则

--- MySuperMarket/App_Start/RouteConfig.cs
+++ MySuperMarket/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,6 +17,13 @@ namespace MySuperMarket
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+                name: "addType",
+                url: "{controller}/{action}/{type}/{id}",
+                defaults: new { controller = "Home", action = "Index", type= UrlParameter.Optional, id = UrlParameter.Optional }
             );
         }
     }
